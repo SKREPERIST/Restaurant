9664ec1e854eb7700ed1130dc7020a68aa847703
SKREPERIST <skreperist666@gmail.com>
2021-03-03 15:50:15 +0200

    added files

---
+++ js/main.js
@@ -0,0 +1,46 @@
+$(function () {
+
+  new WOW().init();
+
+
+  $('.burger').on('click', function () {
+    $('.header__menu').slideToggle();
+  });
+
+  $('.intro__slider').slick();
+
+
+  $('.popular__slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    dots: true,
+    arrows: false,
+    centerMode: true,
+    variableWidth: true,
+    responsive: [{
+        breakpoint: 1230,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          variableWidth: false,
+          centerMode: false
+        }
+      },
+      {
+        breakpoint: 850,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          variableWidth: false,
+          centerMode: false
+        }
+      },
+    ]
+  });
+
+
+  var mixer = mixitup('.choise__box');
+  // var mixer = mixitup('.gall__box');
+
+});
