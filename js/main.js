1c1597cec2023e84132be66841bfca1e74f6f794
SKREPERIST <skreperist666@gmail.com>
2021-03-26 11:04:19 +0200

    final work

--- js/main.js
+++ js/main.js
@@ -1,46 +1,15 @@
 $(function () {
-
-  new WOW().init();
-
-
-  $('.burger').on('click', function () {
-    $('.header__menu').slideToggle();
-  });
-
-  $('.intro__slider').slick();
-
-
-  $('.popular__slider').slick({
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 1,
+  // //////////////////////////////////////////
+  $(".reviews").slick({
+    arrows: true,
     dots: true,
-    arrows: false,
-    centerMode: true,
-    variableWidth: true,
-    responsive: [{
-        breakpoint: 1230,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-          variableWidth: false,
-          centerMode: false
-        }
-      },
-      {
-        breakpoint: 850,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          variableWidth: false,
-          centerMode: false
-        }
-      },
-    ]
   });
+  // /////////////////////////////////////////////
+  let tabs = document.querySelectorAll(".accordion__item ");
 
-
-  var mixer = mixitup('.choise__box');
-  // var mixer = mixitup('.gall__box');
-
+  for (i = 0; i < tabs.length; i++) {
+    tabs[i].onclick = function () {
+      this.classList.toggle("active");
+    };
+  }
 });
