c9ab2ca22b57dbe66d636f4a67278162157e7f31
SKREPERIST <skreperist666@gmail.com>
2021-03-26 11:32:36 +0200

    final work

--- js/main.js
+++ js/main.js
@@ -1,15 +1,46 @@
 $(function () {
-  // //////////////////////////////////////////
-  $(".reviews").slick({
-    arrows: true,
-    dots: true,
-  });
-  // /////////////////////////////////////////////
-  let tabs = document.querySelectorAll(".accordion__item ");
 
-  for (i = 0; i < tabs.length; i++) {
-    tabs[i].onclick = function () {
-      this.classList.toggle("active");
-    };
-  }
+  new WOW().init();
+
+
+  $('.burger').on('click', function () {
+    $('.header__menu').slideToggle();
+  });
+
+  $('.intro__slider').slick();
+
+
+  $('.popular__slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    dots: true,
+    arrows: false,
+    centerMode: true,
+    variableWidth: true,
+    responsive: [{
+        breakpoint: 1230,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          variableWidth: false,
+          centerMode: false
+        }
+      },
+      {
+        breakpoint: 850,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          variableWidth: false,
+          centerMode: false
+        }
+      },
+    ]
+  });
+
+
+  var mixer = mixitup('.choise__box');
+  // var mixer = mixitup('.gall__box');
+
 });
